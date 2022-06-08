e9f3b91255c725a99d1a0ac7d813f8b2b5a1dbf2
Surya4247 <68321732+Surya4247@users.noreply.github.com>
2022-06-09 02:58:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package LockedMe;
+
+public class Main {
+
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\bh\\Desktop\\lockedme"; 
+
+	public static void main(String[] args) {
+		switchcases menu = new switchcases();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+} 
